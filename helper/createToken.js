478090a634f42e8ccc1a9ba66a82a33c8fd3e443
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2025-01-08 14:35:34 -0500

    UPDATEroter.post('/register')w/ updateLoginTimestamp()

--- helper/createToken.js
+++ helper/createToken.js
@@ -3,9 +3,9 @@ const { SECRET_KEY } = require("../config");
 
 /** return signed JWT for payload {username}. */
 
-function createToken(username) {
+createToken = (username) => {
   const payload = { username };
   return jwt.sign(payload, SECRET_KEY);
-}
+};
 
 module.exports = createToken;
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const express = require("express");
 const router = express.Router();
-const { authenticateJWT } = require("../middleware/auth");
+
 const createTokenForUser = require("../helper/createToken");
 
 /** POST /login - login: {username, password} => {token}
@@ -9,6 +9,13 @@ const createTokenForUser = require("../helper/createToken");
  * Make sure to update their last-login!
  *
  **/
+
+router.post("/login", async (req, res, next) => {
+  try {
+  } catch (e) {
+    next(e);
+  }
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -19,6 +26,8 @@ const createTokenForUser = require("../helper/createToken");
 router.post("/register", async (req, res, next) => {
   try {
     const { username, password, first_name, last_name, phone } = req.body;
+
+    //Register the user
     const user = await User.register({
       username,
       password,
@@ -26,7 +35,13 @@ router.post("/register", async (req, res, next) => {
       last_name,
       phone,
     });
+    //Update the login timestamp
+    await User.updateLoginTimestamp(user.username);
+
+    //Generate a token for the user
     const token = createTokenForUser(user.username);
+
+    //Return the token with a 201 status code
     return res.status(201).json({ token });
   } catch (e) {
     return next(e);
